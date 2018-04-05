f295020b3ee378759335ae6792473a5a4253f954
Evan Candler <emcandler21@gmail.com>
2018-04-05 20:42:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    
+
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
@@ -11,9 +11,9 @@ class Application
         resp.write "You Win"
       else
         resp.write "You Lose"
-      end
-   
+    end
+
       resp.finish
     end
-   
+
   end
